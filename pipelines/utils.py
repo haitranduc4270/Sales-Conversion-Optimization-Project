b6df1cb1dd20c0b79e3b5cf333f3ed0a3f5aca96
Dhrubaraj Roy <dhrubarajroy123@gmail.com>
2024-01-05 14:35:34 +0530

    streamlit app has created

--- pipelines/utils.py
+++ pipelines/utils.py
@@ -6,7 +6,7 @@ from src.data_cleaning import DataCleaning, DataPreProcessStrategy
 
 def get_data_for_test():
     try:
-        df = pd.read_csv("/home/miniuser/MLOPS/Sales-Conversion-Optimization-Project/data/raw/KAG_conversion_data.csv")
+        df = pd.read_csv("/home/dhruba/team_project/Sales-Conversion-Optimization-Project/data/raw/KAG_conversion_data.csv")
         df = df.sample(n=100)
         preprocess_strategy = DataPreProcessStrategy()
         data_cleaning = DataCleaning(df, preprocess_strategy)
